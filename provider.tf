0cba4e763a7e2d1ad446e837a6f2c25620ef025b
TarasLeto <taras546@i.ua>
2022-08-17 20:00:27 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
 terraform {
   backend "s3" {
   bucket = "ua-terraform-state"
   key = "dev/ec2-app2/terraform.tfstate"
-  region = "us-east-2"
+  region = "us-east-1"
   }
 }
